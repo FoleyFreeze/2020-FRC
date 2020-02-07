4656cfa71d1d77a6ff1dcea6b543dd927055f3bd
RoboProg <info@foleyfreeze.com>
2020-02-06 21:14:59 -0500

    Created functions in transporter subsystem

--- src/main/java/frc/robot/cals/TransporterCals.java
+++ src/main/java/frc/robot/cals/TransporterCals.java
@@ -3,6 +3,10 @@ package frc.robot.cals;
 public class TransporterCals extends CalSet {
 
     boolean disabled = true;
+    public MotorCal rotateMotor = MotorCal.srx(-1);
+    public int sensorValue;
+    public int launcherValue;
+    public double countsPerIndex = 18.0/32.0/5.0*2048.0;
 
     public TransporterCals(){
 
--- src/main/java/frc/robot/subsystems/Transporter.java
+++ src/main/java/frc/robot/subsystems/Transporter.java
@@ -1,10 +1,62 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.cals.TransporterCals;
+import frc.robot.motors.Motor;
 
 public class Transporter extends SubsystemBase{
-    public Transporter(TransporterCals cals){
 
+    public Motor rotatemotor;
+    public TransporterCals mCals;
+    public DigitalInput ballsensor;
+    public Solenoid launcher;
+    private double targetpos = 0;
+    private boolean[] ballpositions = {false, false, false, false, false};
+    private int ballnumber = 0;
+
+    public Transporter(TransporterCals cals){
+        mCals = cals;
+
+        rotatemotor = Motor.initMotor(mCals.rotateMotor);
+        ballsensor = new DigitalInput(mCals.sensorValue);
+        launcher = new Solenoid(mCals.launcherValue);
+    }
+
+    public void periodic(){
+        rotatemotor.setPosition(targetpos);
+        int x = (int) Math.round(rotatemotor.getPosition() / mCals.countsPerIndex);
+        if(ballpositions[(x + 3) % 5]){
+            ballnumber++;
+            ballpositions[x % 5] = false;
+        }
+    }
+
+    //increment the ball storage
+    public void index(double positions){
+        targetpos += positions * mCals.countsPerIndex;
+    }
+
+    //used for gathering
+    public void autoindex(){
+        double error = targetpos - rotatemotor.getPosition();
+        if(ballsensor.get() && error < mCals.countsPerIndex / 2){
+            ballnumber++;
+            int x = (int) Math.round(targetpos/mCals.countsPerIndex);
+            ballpositions[x % 5] = true;
+            index(1);
+        }
+    }
+
+    //stop shooting
+    public void stoprot(){
+        double x = rotatemotor.getPosition() / mCals.countsPerIndex;
+        x = Math.round(x);
+        targetpos = x * mCals.countsPerIndex;
+    }
+
+    public void enablefire(boolean on){
+        launcher.set(on);
     }
 }
