09ab9684a924268638485900fa068b98aabe39fa
unknown <admin@Robotics34.int.bishopfoley.edu>
2020-02-08 17:20:44 -0500

    tuned pid

--- src/main/java/frc/robot/cals/DriverCals.java
+++ src/main/java/frc/robot/cals/DriverCals.java
@@ -5,27 +5,27 @@ public class DriverCals extends CalSet {
     public boolean disabled = false;
 
     public MotorCal[] driveMotors = {   
-        MotorCal.spark( 1).ramp(0.3).limit(0.2, 0.2).coast(), 
-        MotorCal.spark(14).ramp(0.3).limit(0.2, 0.2).coast(), 
-        MotorCal.spark(15).ramp(0.3).limit(0.2, 0.2).coast(), 
-        MotorCal.spark(20).ramp(0.3).limit(0.2, 0.2).coast()};
+        MotorCal.spark( 1).ramp(0.3).limit(0.5, 0.5).coast(), 
+        MotorCal.spark(14).ramp(0.3).limit(0.5, 0.5).coast(), 
+        MotorCal.spark(15).ramp(0.3).limit(0.5, 0.5).coast(), 
+        MotorCal.spark(20).ramp(0.3).limit(0.5, 0.5).coast()};
     public MotorCal[] turnMotors = {   
-        MotorCal.spark(11).pid(0.00003, 0, 0.0000, 0).dFilt(0.3).limit(0.2, 0.2).brake().ramp(0.3), 
-        MotorCal.spark( 4).pid(0.00003, 0, 0.0000, 0).dFilt(0.3).limit(0.2, 0.2).brake().ramp(0.3), 
-        MotorCal.spark( 5).pid(0.00003, 0, 0.0000, 0).dFilt(0.3).limit(0.2, 0.2).brake().ramp(0.3), 
-        MotorCal.spark(10).pid(0.00003, 0, 0.0000, 0).dFilt(0.3).limit(0.2, 0.2).brake().ramp(0.3)};
+        MotorCal.spark(11).pid(0.002, 0, 0.004, 0).dFilt(0.01).limit(0.6, 0.6).brake().ramp(0.001), 
+        MotorCal.spark( 4).pid(0.002, 0, 0.004, 0).dFilt(0.01).limit(0.6, 0.6).brake().ramp(0.001), 
+        MotorCal.spark( 5).pid(0.002, 0, 0.004, 0).dFilt(0.01).limit(0.6, 0.6).brake().ramp(0.001), 
+        MotorCal.spark(10).pid(0.002, 0, 0.004, 0).dFilt(0.01).limit(0.6, 0.6).brake().ramp(0.001)};
     public int[] turnEncoderIds = {2, 1, 3, 0};
     public double[] xPos = {-12.375, 12.375, -12.375, 12.375};
     public double[] yPos = {10.625, 10.625, -10.625, -10.625};
     public double[] angleOffset;
 
     //this is for 2019 prac bot
-    //public double turnGearRatio = 20.0/60.0 * 20.0/60.0 * 40.0/64.0;
-    public double turnGearRatio = 1.0/60.0;
+    //public double turnGearRatio = 20.0/60.0 * 20.0/60.0 * 40.0/64.0 / 4096.0;
+    public double turnGearRatio = 60.0 * 42.0;
 
     public double pausePwrPne = 0.9;//driving over this power will pause compressor
 
-    public double parkOffset = 99999;//0.5;
+    public double parkOffset = 0.5;
 
     public double driveStraightKp = 0;//0.05;//100% after abt 100deg of error
     
@@ -42,12 +42,14 @@ public class DriverCals extends CalSet {
 
         switch(type){
             case COMPETITION:
-                angleOffset = new double[]{0.555,3.044,4.715,3.054};
+                                        //these are in encoder order (not wheel order) 
+                angleOffset = new double[]{3.052,3.037,0.536,2.168};
             break;
 
             case PRACTICE:
                 angleOffset = new double[]{0, 0, 0, 0};
             break;
+
         }
 
     }
