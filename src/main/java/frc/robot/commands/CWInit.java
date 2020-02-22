fd79b85d8c0085fad0ddf7f34035ce8e3ceb2f31
RoboProg <info@foleyfreeze.com>
2020-02-22 11:18:59 -0500

    CW set up

--- src/main/java/frc/robot/commands/CWInit.java
+++ src/main/java/frc/robot/commands/CWInit.java
@@ -3,27 +3,28 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 
-public class CWSpin extends CommandBase{
+public class CWInit extends CommandBase{
+    
+    RobotContainer m_subsystem;
 
-    private RobotContainer m_subsystem;
-
-    public CWSpin(RobotContainer subsystem){
+    public CWInit(RobotContainer subsystem){
         m_subsystem = subsystem;
         addRequirements(m_subsystem.m_transporterCW);
     }
 
     @Override
     public void initialize(){
-
+        //m_subsystem.m_transporterCW.
     }
 
     @Override
     public void execute(){
-        
+
     }
 
     @Override
     public void end(boolean interrupted){
+
     }
 
     @Override
--- src/main/java/frc/robot/commands/CWMove.java
+++ src/main/java/frc/robot/commands/CWMove.java
@@ -3,13 +3,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 
-public class CWSpin extends CommandBase{
+public class CWMove extends CommandBase{
+    
+    RobotContainer m_subsystem;
 
-    private RobotContainer m_subsystem;
+    public void CWMove(){
 
-    public CWSpin(RobotContainer subsystem){
-        m_subsystem = subsystem;
-        addRequirements(m_subsystem.m_transporterCW);
     }
 
     @Override
@@ -19,11 +18,12 @@ public class CWSpin extends CommandBase{
 
     @Override
     public void execute(){
-        
+
     }
 
     @Override
     public void end(boolean interrupted){
+
     }
 
     @Override
