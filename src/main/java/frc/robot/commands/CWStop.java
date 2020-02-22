ed1f1f9bdf2ebeed571bb74e273bacba98bafbff
RoboProg <info@foleyfreeze.com>
2020-02-22 11:18:59 -0500

    CW set up

--- src/main/java/frc/robot/commands/CWStop.java
+++ src/main/java/frc/robot/commands/CWStop.java
@@ -3,13 +3,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 
-public class CWSpin extends CommandBase{
+public class CWStop extends CommandBase{
 
-    private RobotContainer m_subsystem;
+    RobotContainer m_subsystem;
 
-    public CWSpin(RobotContainer subsystem){
+    public void CWStop(RobotContainer subsystem){
         m_subsystem = subsystem;
-        addRequirements(m_subsystem.m_transporterCW);
     }
 
     @Override
@@ -19,11 +18,12 @@ public class CWSpin extends CommandBase{
 
     @Override
     public void execute(){
-        
+
     }
 
     @Override
     public void end(boolean interrupted){
+        
     }
 
     @Override
